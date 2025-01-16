e81c9015e89c5bc83b1ea0284b8c67a4df92abc1
Jack05325 <giaco.demitri@gmail.com>
2025-01-16 22:47:33 +0100

    bug fixing

--- readQRCode.py
+++ readQRCode.py
@@ -23,9 +23,10 @@ def saveCSV():
         writer.writerows(data_list)  # Scrivi le righe modificate
 
 reloadCSV()
+cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("img", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
-
 while True:
     _, img = cap.read()
     # detect and decode
@@ -33,20 +34,18 @@ while True:
     # check if there is a QRCode in the image
     if bbox is not None:
         reloadCSV()
-        if data:
-            print("[+] QR Code detected, data:", data)
         found = False  # Variabile per monitorare se troviamo il valore
         bboxTmp = bbox[0]
         for person in data_list:
             if data == person['OrderNumber']:
-                print("[+] QR Code is valid")
-                print("[+] Nome:", person["Nome"])
+                #print("[+] QR Code is valid")
+                #print("[+] Nome:", person["Nome"])
                 person["IsPresent"] = "1"
                 found = True
                 
                 for i in range(len(bboxTmp)):
                     nextPointIndex = (i+1) % len(bboxTmp)
-                    print(bboxTmp[i])
+                    #print(bboxTmp[i])
                     cv2.line(img, tuple(map(int, bboxTmp[i])), tuple(map(int, bboxTmp[nextPointIndex])), (0,255,0), 5)
                 
                 x, y = map(int, bbox[0][0])  # Coordinate dell'angolo superiore sinistro del QR Code
@@ -57,16 +56,18 @@ while True:
             x, y = map(int, bbox[0][0])  # Coordinate dell'angolo superiore sinistro del QR Code
             for i in range(len(bboxTmp)):
                     nextPointIndex = (i+1) % len(bboxTmp)
-                    print(bboxTmp[i])
+                    #print(bboxTmp[i])
                     cv2.line(img, tuple(map(int, bboxTmp[i])), tuple(map(int, bboxTmp[nextPointIndex])), (0,0,255), 5)
             cv2.putText(img, "QR Code non valido", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-            print("[-] QR Code is not valid")
+            #print("[-] QR Code is not valid")
     cv2.rectangle(img, tuple(startPoint), (500, img.shape[0]) , (0, 0, 0), thickness)
     posText = 0
+    indice = 1
     for data in data_list:
         if data["IsPresent"] == "1":  
             posText += 50        
-            cv2.putText(img, data["Nome"], tuple([0, posText]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)            
+            cv2.putText(img, f"{indice} {data['Nome']}", tuple([0, posText]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)   
+            indice += 1
     cv2.imshow("img", img)
     if cv2.waitKey(1) == ord("q"):
         break
